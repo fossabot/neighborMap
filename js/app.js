f3edc05902f86ac0f8b5fb25452af7301e8463b6
zlav <zcjlavallee@gmail.com>
2017-10-05 10:13:13 -0500

    List search added

--- js/app.js
+++ js/app.js
@@ -5,13 +5,26 @@ var markers = [];
 //Knockout JS Javascript and connection to the Google Maps API
 var initialMarkers = [
 {
-	position: {lat: 41.917393, lng: -87.644555},
-	title: 'Home'
-},
-{
 	position: {lat: 41.914000, lng: -87.644555},
 	title: 'Not Home'
+},
+{
+	position: {lat: 41.922572, lng: -87.644352},
+	title: 'Allende #2'
+},
+{
+	position: {lat: 41.921092, lng: -87.633991},
+	title: 'Zoo'
+},
+{
+	position: {lat: 41.921658, lng: -87.652406},
+	title: 'State'
+},
+{
+	position: {lat: 41.904206, lng: -87.634281},
+	title: 'Happy Camper'
 }
+
 ]
 
 var ListItem = function(data, identification) {
@@ -27,29 +40,70 @@ var ViewModel = function(){
 	var i = 1;
 
 	var infoWindow = new google.maps.InfoWindow();
+	var defaultIcon = makeMarkerIcon('009100');
+	var highlightedIcon = makeMarkerIcon('FFFF24');
 	
 	initialMarkers.forEach(function(newMarker){
 		self.list.push(new ListItem(newMarker, i++));
+
 		var marker = new google.maps.Marker({
 			map: map,
             position: newMarker.position,
             title: newMarker.title,
             animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            id: i
           });
 		markers.push(marker);
-
 		marker.addListener('click', function(){
 			populateInfoWindow(this, infoWindow);
 		});
 
+		//Mouseover and off for markers
+		marker.addListener('mouseover', function() {
+        	this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+   		     this.setIcon(defaultIcon);
+        });
+
 	});
+	
 
 	this.selectMarker = function(selMarker){
 		//Markers mapped to the list by ID so subtract 1
 		populateInfoWindow(markers[selMarker.id-1], infoWindow);	
 	};
-}
 
+	//Mouseover and off for list items
+	this.highlight = function(selMarker){
+		markers[selMarker.id-1].setIcon(highlightedIcon);
+	};
+	this.dehighlight = function(selMarker){
+		markers[selMarker.id-1].setIcon(defaultIcon);
+	};
+
+
+  
+	self.filterText = ko.observable("");
+	self.filteredItems = ko.computed(function(){
+		var reg;
+		fText = self.filterText().replace(/\|\s*$/gi, '|');
+		fText = fText.replace(/\|\s*$/gi, '');
+		console.log('regex: ', fText);
+		reg = new RegExp(fText, "gi");
+
+		var filtered = ko.utils.arrayFilter(self.list(), function(item){
+			if(fText.length){
+				return ko.utils.unwrapObservable(item.title).match(reg);
+			}
+			else{
+				return 1;
+			}
+		});
+		return filtered;
+	}, self);
+}
 //Run on maps API callback
 function initMap(){
 
@@ -64,21 +118,52 @@ function initMap(){
 
 //Make the info window
 function populateInfoWindow(marker, infowindow) {
-
-	//Open it if it is not open
 	if (infowindow.marker != marker){
+		var client_secret = "05KGY0XGXG2JY1UVH5BT5N4Y52N2BXOHNGQJQZJYVCSG2EFJ";
+		var client_id = "AIH3NAKDXUPPLFQAHW1TIVXKVSGSZVRZL1FQYKU5FYTU2VN1";
+		var url = '';
+		var v = new Date().getTime();
+		url += "https://api.foursquare.com/v2/venues/search?";
+		url += $.param({'query': marker.title, 'client_id': client_id, 'client_secret': client_secret, 'v': v,});
+		url += '&' + $.param({'ll':marker.getPosition().lat() + ',' +marker.getPosition().lng(), 'limit': 2, 'radius': 5});
+		console.log(url);
+
 		infowindow.marker = marker;
-		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.setContent('<div><a id="fourlink" target="_blank">' + marker.title + '</a></div><br><div id="foursquare"></div>');
+		$.getJSON(url, function (data) {
+			console.log(data);
+			if(data.meta.code == 200){
+				document.getElementById('foursquare').innerHTML = data.response.venues[0].contact.formattedPhone;
+				document.getElementById('fourlink').href = data.response.venues[0].url;
+			};
+		}).fail(function(jqxhr, textStatus, error){
+			console.log('error');
+		});
+
+		//Open it if it is not open
 		infowindow.open(map, marker);
 		infowindow.addListener('closeclick', function(){
 			infowindow.marker = null;
 		});
 	}
+
 	//Close it if it is already open
 	else{
 		infowindow.marker = null;
 		infowindow.close();
 	}
+}
 
-
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+		'|40|_|%E2%80%A2',
+		new google.maps.Size(21, 34),
+		new google.maps.Point(0, 0),
+		new google.maps.Point(10, 34),
+		new google.maps.Size(21,34));
+	return markerImage;
 }
