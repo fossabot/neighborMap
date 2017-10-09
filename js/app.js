e3a587677f0c7996df3c1f318cecdafc74975855
zlav <zcjlavallee@gmail.com>
2017-10-09 12:01:55 -0500

    Semicolon added

--- js/app.js
+++ js/app.js
@@ -216,5 +216,5 @@ window.addEventListener('error', function(e) {
     var ie = window.event || {};
     var errMsg = e.message || ie.errorMessage || "404 error on " + window.location;
     var errSrc = (e.filename || ie.errorUrl) + ': ' + (e.lineno || ie.errorLine);
-    window.alert("Error loading Google Maps API")
+    window.alert("Error loading Google Maps API");
 }, true);
